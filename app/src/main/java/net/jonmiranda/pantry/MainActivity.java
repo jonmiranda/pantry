f94ea6bd4cba7e8d2a7cbfae9285f43f22488107
Jon Miranda <miranda.jra@gmail.com>
2015-10-22 11:21:21 -0700

    Added basic pantry list.

--- app/src/main/java/net/jonmiranda/pantry/MainActivity.java
+++ app/src/main/java/net/jonmiranda/pantry/MainActivity.java
@@ -1,16 +1,33 @@
 package net.jonmiranda.pantry;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String[] ITEMS = new String[] {"Apples", "Bananas", "Carrots"};
+
+    @Bind(R.id.pantry_list_view) RecyclerView pantryListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        pantryListView.setLayoutManager(new LinearLayoutManager(this));
+        pantryListView.setAdapter(new PantryListAdapter(ITEMS));
     }
 
     @Override
@@ -33,5 +50,40 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    static class PantryListAdapter extends RecyclerView.Adapter<PantryItemViewHolder> {
+
+        private final String[] items;
+
+        public PantryListAdapter(String[] items) {
+            this.items = items;
+        }
+
+        @Override
+        public PantryItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+              .inflate(R.layout.pantry_list_view_item, parent, false);
+            return new PantryItemViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(PantryItemViewHolder holder, int position) {
+            holder.itemName.setText(items[position]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return items.length;
+        }
+    }
+
+    static class PantryItemViewHolder extends RecyclerView.ViewHolder {
+        protected TextView itemName;
+
+        public PantryItemViewHolder(View view) {
+            super(view);
+            itemName = (TextView) view.findViewById(R.id.item_name);
+        }
     }
 }
