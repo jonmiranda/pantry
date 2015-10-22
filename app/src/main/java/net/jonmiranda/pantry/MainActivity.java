4a9263b03f5966346592722a503ac1328f435be7
Jon Miranda <miranda.jra@gmail.com>
2015-10-22 11:23:04 -0700

    Code clean up. Removed comments.

--- app/src/main/java/net/jonmiranda/pantry/MainActivity.java
+++ app/src/main/java/net/jonmiranda/pantry/MainActivity.java
@@ -32,28 +32,20 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
     static class PantryListAdapter extends RecyclerView.Adapter<PantryItemViewHolder> {
-
         private final String[] items;
 
         public PantryListAdapter(String[] items) {
